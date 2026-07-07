3f482d075b3146ad5acdb35c48517545a14edd5b
velpa22 <4v82z7j2gj@privaterelay.appleid.com>
2026-07-07 16:55:02 +0100

    Create calculs.py
    Jour 1 : Développement validé - Semaine 1

---
+++ Prechauffage_Semaine1/calculs.py
@@ -0,0 +1,10 @@
+# SEMAINE 1 - DEVELOPPEMENT
+# Exercices Yvan Monka
+
+exo1 = "3(x + 2) = 3x + 6"
+exo2 = "5(2x - 1) = 10x - 5" 
+exo3 = "4x(x + 3) = 4x² + 12x"
+exo4 = "2(x - 7) = 2x - 14"
+exo5 = "x(5x + 2) = 5x² + 2x"
+
+print("Jour 1 validé. REMONTADA en cours.")
