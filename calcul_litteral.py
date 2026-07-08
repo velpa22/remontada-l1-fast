f490d3bd3dfba27033e7848457707b80e54a8ebc
velpa22 <4v82z7j2gj@privaterelay.appleid.com>
2026-07-08 15:15:59 +0100

    Create calcul_litteral.py

---
+++ calcul_litteral.py
@@ -0,0 +1,12 @@
+# Jour 2 - Calcul littéral - 06/07/2026
+
+# Exo 1: 5x + 3x = 8x
+# Exo 2: 2(x + 4) = 2x + 8  
+# Exo 3: 7y - 2y + 5 = 5y + 5
+# Exo 4: 3(a - 2) + 4a = 7a - 6
+# Exo 5: 9x - 4x = 5x
+# Exo 6: 5(y + 3) = 5y + 15
+# Exo 7: 2a + 7 - a = a + 7
+# Exo 8: 4(b - 1) + 2b = 6b - 4
+# Exo 9: 3x + 2x - 5 = 5x - 5
+# Exo 10: 6(m + 2) - 3m = 3m + 12
